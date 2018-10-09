c531f1cc7e16e0fd663ff79ae32bc79e3661a08e
Priam123 <13pramo@gmail.com>
2018-10-09 11:30:57 +0530

    Wireless update

--- IoTBLE/app/src/main/java/com/example/researcher/iot_ble/OfferActivity.java
+++ IoTBLE/app/src/main/java/com/example/researcher/iot_ble/OfferActivity.java
@@ -3,38 +3,41 @@ package com.example.researcher.iot_ble;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.util.ArrayList;
+import java.util.List;
 
 import cz.msebera.android.httpclient.Header;
 
 public class OfferActivity extends AppCompatActivity {
-
+    List<String> list = new ArrayList<String>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_offer);
         Intent i =getIntent();
         String MACAddr = i.getStringExtra("MACAddr");
+        Toast.makeText(OfferActivity.this,MACAddr,Toast.LENGTH_LONG).show();
+
+        ListView l = (ListView)findViewById(R.id.list);
         final TextView offerId = (TextView)findViewById(R.id.offerId);
-        final TextView showText = (TextView)findViewById(R.id.showText);
 
         AsyncHttpClient httpClient = new AsyncHttpClient();
         httpClient.get("https://harsha555.pythonanywhere.com/iot_ggwp?id=" + MACAddr, new AsyncHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+
                 if(responseBody!=null) {
+
                     String response = new String(responseBody);
                     offerId.setText(response);
+                    /*
                     ArrayList<OfferJson> offerJsonArrayList = new ArrayList<>();
 
                     try {
@@ -45,9 +48,10 @@ public class OfferActivity extends AppCompatActivity {
                             String itemName = offerList.getString("item");
                             String discountVal = offerList.getString("discount");
                             OfferJson offerJson = new OfferJson();
-                            offerJson.setItem(itemName);
+                            offerJson.setItem(itemName +" : "+itemName);
                             offerJson.setDiscount(discountVal);
                             offerJsonArrayList.add(offerJson);
+                            list.add(itemName+ ":" +discountVal);
                         }
                     } catch (JSONException e) {
                         e.printStackTrace();
@@ -55,6 +59,7 @@ public class OfferActivity extends AppCompatActivity {
 
 
                     Toast.makeText(OfferActivity.this,offerJsonArrayList.get(0).toString(),Toast.LENGTH_LONG).show();
+                    */
                 }
 
             }
